565dde2349f16c5a19018e65815791b02554fe9f
Hasan Imran <hasanimran4095009@gmail.com>
2024-09-26 13:59:22 +0300

    First commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,40 +1,50 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+    <h1>ToDo list</h1>
+    <input v-model= "newTask" placeholder="Add new Task"/>
+    <button @click="addTodo">Add task</button>
+
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+    <li v-for="todo in todos" :key="todo.id">
+    {{todo.task}}
+    <button @click="deleteTodo(todo.id)">Delete task</button>
+    </li>
     </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      todos: [],
+      newTask: ''
+    }
+  },
+  created() {
+    this.fetchTodos()
+  },
+  methods: {
+    async fetchTodos() {
+      const response = await axios.get('http://localhost:3000/api/todos')  // Updated URL
+      this.todos = response.data
+      console.log(response.data);
+    },
+    async addTodo() {
+      if (!this.newTask) return
+      const response = await axios.post('http://localhost:3000/api/todos', {
+        task: this.newTask    //<!--we are sending payload to the backend API in payload-->
+       
+      })
+      this.todos.push(response.data)
+      this.newTask = ''
+    },
+    async deleteTodo(id) {
+      await axios.delete(`http://localhost:3000/api/todos/${id}`)  // Updated URL
+      this.todos = this.todos.filter(todo => todo.id !== id)
+    }
   }
 }
 </script>
@@ -55,4 +65,16 @@ li {
 a {
   color: #42b983;
 }
+input
+{
+padding: 5px 10px;
+}
+button
+{
+margin-left: 10px;
+padding: 5px 10px;
+cursor: pointer;
+background-color: #ff6666;
+}
 </style>
+
